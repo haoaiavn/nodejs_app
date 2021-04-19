7a16464e039e08252797cbf865b459a3403f109f
hao <hao.aiavn@gmail.com>
2021-04-20 01:47:25 +0700

    Init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express()
+const port = 3000;
+
+app.get('/', (req, res) => {
+  var a = 1;
+  res.send('hello world!')
+});
+
+app.listen(port, ()=> console.log(`Listening at http://localhost:${port}`));
